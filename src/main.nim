c51c0fc51186c9d4430cee555b6bd72b9f1953f5
Z'vonty Flugence <zvontyflugence@gmail.com>
2018-02-19 19:54:39 -0500

    Update main.nim
    Added better exception handling, added custom controls (MenuLabel and MenuButton), among other small changes.

--- src/main.nim
+++ src/main.nim
@@ -1,5 +1,8 @@
 import nigui
 import streams
+import strutils
+import menu_button
+import menu_label
 
 app.init()
 
@@ -8,45 +11,62 @@ window.width = 800
 window.height = 600
 
 var mainContainer = newLayoutContainer(Layout_Vertical)
+mainContainer.backgroundColor = rgb(23, 25, 33)
 window.add(mainContainer)
 
 var menuContainer = newLayoutContainer(Layout_Horizontal)
+menuContainer.backgroundColor = rgb(23, 25, 33)
 mainContainer.add(menuContainer)
 
+var fileName = newMenuLabel()
+fileName.width = 100
+fileName.height = 20
+mainContainer.add(fileName)
+
 var textArea = newTextArea()
+textArea.backgroundColor = rgb(23, 25, 33)
 mainContainer.add(textArea)
 
-var openFileBtn = newButton("Open File")
+var newFileBtn = newMenuButton("New File")
+menuContainer.add(newFileBtn)
+newFileBtn.onClick = proc(event: ClickEvent) =
+    textArea.text = ""
+    fileName.text= "New File" 
+
+var openFileBtn = newMenuButton("Open File")
 menuContainer.add(openFileBtn)
-openFileBtn.onClick = proc(event: ClickEvent) = 
+openFileBtn.onClick = proc(event: ClickEvent) =
     var dialog = newOpenFileDialog()
-    dialog.title = "Select File"
-    dialog.multiple = false
-    dialog.directory = "/test"
-    dialog.run()
-    try:
-        var fs = newFileStream(dialog.files[0], fmRead)
-        var line = ""
-        var fileContent = ""
-        if not isNil(fs):
+    dialog.title = "Select File"    
+    dialog.multiple = false    
+    dialog.directory = "/test"    
+    dialog.run()    
+    try:        
+        var fs = newFileStream(dialog.files[0], fmRead)        
+        var line = ""        
+        var fileContent = ""        
+        if not isNil(fs):            
             while fs.readLine(line):
-                add(fileContent, line)
-            fs.close()
-            textArea.text = fileContent
-    except IndexError:
+                add(line, "\n")              
+                add(fileContent, line)            
+            fs.close()            
+            textArea.text = fileContent            
+            var pathParts = split(dialog.files[0], "\\")            
+            fileName.text= pathParts[len(pathParts)-1]
+    except IndexError:       
         window.alert("Error: Invalid file!")
 
-var saveFileBtn = newButton("Save File")
+var saveFileBtn = newMenuButton("Save File")
 menuContainer.add(saveFileBtn)
 saveFileBtn.onClick = proc(event: ClickEvent) =
     var dialog = newSaveFileDialog()
     dialog.title = "Save File"
     dialog.directory = "/test"
     dialog.run()
-    if dialog.file == "":
-        window.alert("Error: Save Failed!")
-    else:
-        writeFile(dialog.file, textArea.text)
-
+    try:
+        writeFile(dialog.file, textArea.text)    
+    except:        
+        window.alert("Error: Save failed!")
+        
 window.show()
 app.run()
