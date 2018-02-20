785a446d01a7363732ce30969ec6b39cc5ccb4b2
Z'vonty Flugence <zvontyflugence@gmail.com>
2018-02-19 19:51:56 -0500

    Create menu_button.nim
    Added custom NiGui Button Control, called MenuButton that allows for greater color customization.

---
+++ src/menu_button.nim
@@ -0,0 +1,17 @@
+import nigui
+
+type MenuButton* = ref object of Button
+
+proc newMenuButton*(text = ""): MenuButton =
+    result  = new MenuButton
+    result.init()
+    result.text = text
+
+method handleDrawEvent*(control: MenuButton, event: DrawEvent) =
+    let canvas = event.control.canvas
+    canvas.areaColor = rgb(23, 25, 33)
+    canvas.textColor = rgb(255, 233, 83)
+    canvas.lineColor = rgb(255, 233, 83)
+    canvas.drawRectArea(0, 0, control.width, control.height)
+    canvas.drawTextCentered(control.text)
+    #canvas.drawRectOutline(0, 0, control.width, control.height)
